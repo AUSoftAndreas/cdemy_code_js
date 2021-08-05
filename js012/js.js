fd6720492ec32dfed1f6b567e32dbd7baca4d8be
Andreas Hoefer <hoefer74@gmail.com>
2021-08-06 00:29:49 +0200

    x

---
+++ js012/js.js
@@ -0,0 +1,46 @@
+var players = [
+    ["Neuer", "TW"],
+    ["Upamecano", "V"],
+    ["Hernandez", "V"],
+    ["Davies", "V"],
+    ["Parvard", "V"],
+    ["Kimmich", "M"],
+    ["Goretzka", "M"],
+    ["Müller", "M"],
+    ["Sane", "A"],
+    ["Gnabry", "A"],
+    ["Lewandowski", "A"]
+];
+
+chance();
+
+function chance() {
+    let player1;
+    let player2;
+    let player3;
+    let rnd = Math.floor(Math.random() * players.length);
+    player1 = players[rnd];
+    while (!player2) {
+        rnd = Math.floor(Math.random() * players.length);
+        if (players[rnd] != player1) {
+            if (players[rnd][1] == "M" || players[rnd][1] == "A") {
+                player2 = players[rnd];
+            }
+        }
+    }
+    while (!player3) {
+        rnd = Math.floor(Math.random() * players.length);
+        if (players[rnd] != player1 && players[rnd] != player2) {
+            if (players[rnd][1] == "M" || players[rnd][1] == "A") {
+                player3 = players[rnd];
+            }
+        }
+    }
+    console.log(player1[0] + " gibt den Ball weiter an " + player2[0] + ". Der leitet weiter an " + player3[0]);
+    rnd = Math.random();
+    if (rnd < 0.5) {
+        console.log("---> Leider vorbei");
+    } else {
+        console.log("--> TOOOOOOR!!!!");
+    }
+}
---
+++ js013/js.js
@@ -0,0 +1,46 @@
+var players = [
+    ["Neuer", "TW"],
+    ["Upamecano", "V"],
+    ["Hernandez", "V"],
+    ["Davies", "V"],
+    ["Parvard", "V"],
+    ["Kimmich", "M"],
+    ["Goretzka", "M"],
+    ["Müller", "M"],
+    ["Sane", "A"],
+    ["Gnabry", "A"],
+    ["Lewandowski", "A"]
+];
+
+chance();
+
+function chance() {
+    let player1 = getPlayer();
+    let player2 = getPlayerOffensiv();
+    let player3 = getPlayerOffensiv();
+    console.log(player1[0] + " gibt den Ball weiter an " + player2[0] + ". Der leitet weiter an " + player3[0]);
+    if (getTor()) {
+        console.log("--> TOOOOOOR!!!!");
+    } else {
+        console.log("---> Leider vorbei");
+    }
+}
+
+function getPlayer() {
+    let rnd = Math.floor(Math.random() * players.length);
+    return players[rnd];
+}
+
+function getPlayerOffensiv() {
+    let player = getPlayer();
+    if (player[1] == "A" || player[1] == "M") {
+        return player;
+    } else {
+        return getPlayerOffensiv();
+    }
+}
+
+function getTor() {
+    let rnd = Math.random();
+    return (rnd < 0.5);
+}
