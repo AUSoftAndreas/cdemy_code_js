b59c1989a3e121721fbc2587521630fcd3c2dae4
cdemy Andreas Höfer <info@cdemy.de>
2021-08-12 17:46:26 +0200

    x

--- js012/js.js
+++ js012/js.js
@@ -12,35 +12,32 @@ var players = [
     ["Lewandowski", "A"]
 ];
 
-chance();
 
-function chance() {
-    let player1;
-    let player2;
-    let player3;
-    let rnd = Math.floor(Math.random() * players.length);
-    player1 = players[rnd];
-    while (!player2) {
-        rnd = Math.floor(Math.random() * players.length);
-        if (players[rnd] != player1) {
-            if (players[rnd][1] == "M" || players[rnd][1] == "A") {
-                player2 = players[rnd];
-            }
+let player1;
+let player2;
+let player3;
+let rnd = Math.floor(Math.random() * players.length);
+player1 = players[rnd];
+while (!player2) {
+    rnd = Math.floor(Math.random() * players.length);
+    if (players[rnd] != player1) {
+        if (players[rnd][1] == "M" || players[rnd][1] == "A") {
+            player2 = players[rnd];
         }
     }
-    while (!player3) {
-        rnd = Math.floor(Math.random() * players.length);
-        if (players[rnd] != player1 && players[rnd] != player2) {
-            if (players[rnd][1] == "M" || players[rnd][1] == "A") {
-                player3 = players[rnd];
-            }
+}
+while (!player3) {
+    rnd = Math.floor(Math.random() * players.length);
+    if (players[rnd] != player1 && players[rnd] != player2) {
+        if (players[rnd][1] == "M" || players[rnd][1] == "A") {
+            player3 = players[rnd];
         }
     }
-    console.log(player1[0] + " gibt den Ball weiter an " + player2[0] + ". Der leitet weiter an " + player3[0]);
-    rnd = Math.random();
-    if (rnd < 0.5) {
-        console.log("---> Leider vorbei");
-    } else {
-        console.log("--> TOOOOOOR!!!!");
-    }
+}
+console.log(player1[0] + " gibt den Ball weiter an " + player2[0] + ". Der leitet weiter an " + player3[0]);
+rnd = Math.random();
+if (rnd < 0.5) {
+    console.log("---> Leider vorbei");
+} else {
+    console.log("--> TOOOOOOR!!!!");
 }
