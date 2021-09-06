0d0cf3e43350dbd363d6422fb692d777cd899bb0
cdemy Leitung <88116396+cdemyLeitung@users.noreply.github.com>
2021-09-06 15:57:21 +0200

    dart übungen

---
+++ dart_intro_001/dart.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+
+void main() {
+  var a = 1;
+  var b = 'Hallo';
+  var c = 7 / 3;
+  var d = Random();
+  // beliebig Code dazwischen
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+}
