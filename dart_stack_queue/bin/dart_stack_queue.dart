ba2ace13db5ce4dbfae8605c53ab726ec36debdb
AUSoftAndreas <andreas_hoefer@cdemy.de>
2022-08-10 00:10:26 +0200

    unnötiges entfernt

--- dart_stack_queue/bin/dart_stack_queue.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:dart_stack_queue/dart_stack_queue.dart' as dart_stack_queue;
-
-// void main(List<String> arguments) {
-//   print('Hello world: ${dart_stack_queue.calculate()}!');
-// }
